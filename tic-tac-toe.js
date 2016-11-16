4484070a548c750e9b94cf7bea1e23bb931357bd
Michael <di8848mi0210@pusd.us>
2016-11-16 10:18:02 -0800

    Started the shapes

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,10 +5,10 @@ var namespace = "http://www.w3.org/2000/svg"
 function makeShape() {
   //code to create a shape
   var circle = document.createElementNS(namespace, "circle")
-  circle.setAttribute("cx", 15)
-  circle.setAttribute("cy", 0)
-  circle.setAttribute("r",10)
+  circle.setAttribute("cx",58)
+  circle.setAttribute("cy", 43)
+  circle.setAttribute("r",25)
   circle.setAttribute("fill","red")
-  var canvas = document.getElementById("my-canvas")
+  var canvas = document.getElementById("game-board")
   canvas.appendChild(circle)
 }
