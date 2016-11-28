0fa8e2c90d00e4c373c776fb6179b2d21e4efe42
Michael <di8848mi0210@pusd.us>
2016-11-28 11:27:46 -0800

    Trouble

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,16 +2,34 @@
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
 
-function makeShape() {
-  //code to create a shape
-  var circle = document.createElementNS(namespace, "circle")
-  circle.setAttribute("cx",58)
-  circle.setAttribute("cy", 43)
-  circle.setAttribute("r",25)
-  circle.setAttribute("fill","red")
-  var canvas = document.getElementById("game-board")
-  canvas.appendChild(circle)
+var turn= "player1"
+
+function  makeShape(){
+  if(turn =="player1"){
+    var circle = document.createElementNS(namespace, "circle")
+    circle.setAttribute("cx",58)
+    circle.setAttribute("cy", 43)
+    circle.setAttribute("r",25)
+    circle.setAttribute("fill","red")
+    var canvas = document.getElementById("game-board")
+    canvas.appendChild(circle)
+    turn="player2"
+  }
+}else{
+  //Create rect in top left
+  var rect = document.createElementNS(namespace, "rect")
+  rect.setAttribute("x",15)
+  rect.setAttribute("y" 0)
+  rect.setAttribute("width" 88)
+  rect.setAttribute("height" 90)
+  rect.setAttribute("fill","aqua" )
+  var canvas =document.getElementById("game-board")
+  canvas.appendChild(rect)
+  turn="player1"
+  }
 }
+
+
 
 function changeShape() {
   //code to create a shape
@@ -30,7 +48,7 @@ function anotherShape() {
   circle.setAttribute("cx",255)
   circle.setAttribute("cy", 43)
   circle.setAttribute("r",25)
-  circle.setAttribute("fill","red")
+  circle.setAttribute("fill","blue")
   var canvas = document.getElementById("game-board")
   canvas.appendChild(circle)
 }
@@ -63,7 +81,7 @@ function MakeAnotherShape() {
   circle.setAttribute("cx",160)
   circle.setAttribute("cy", 148)
   circle.setAttribute("r",25)
-  circle.setAttribute("fill","red")
+  circle.setAttribute("fill","blue")
   var canvas = document.getElementById("game-board")
   canvas.appendChild(circle)
 }
@@ -74,7 +92,7 @@ function AnotherOne() {
   circle.setAttribute("cx",60)
   circle.setAttribute("cy", 245)
   circle.setAttribute("r",25)
-  circle.setAttribute("fill","red")
+  circle.setAttribute("fill","blue")
   var canvas = document.getElementById("game-board")
   canvas.appendChild(circle)
 }
@@ -96,7 +114,16 @@ function LastShape() {
   circle.setAttribute("cx",255)
   circle.setAttribute("cy", 245)
   circle.setAttribute("r",25)
-  circle.setAttribute("fill","red")
+  circle.setAttribute("fill","blue")
   var canvas = document.getElementById("game-board")
   canvas.appendChild(circle)
+}
+
+
+var turn= "player1"
+
+function  makeShape(){
+  if(turn=="player1"){
+
+  }
 }
