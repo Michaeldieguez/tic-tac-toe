60e8fcf5d39c78cfe29eba32bfb3895f8505b159
Michael <di8848mi0210@pusd.us>
2016-12-15 10:17:50 -0800

    Done

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,7 +5,6 @@ var namespace = "http://www.w3.org/2000/svg"
 var turn= "player1"
 
 function  makeShape(){
-  if(turn =="player1"){
     var circle = document.createElementNS(namespace, "circle")
     circle.setAttribute("cx",58)
     circle.setAttribute("cy", 43)
@@ -13,22 +12,7 @@ function  makeShape(){
     circle.setAttribute("fill","red")
     var canvas = document.getElementById("game-board")
     canvas.appendChild(circle)
-    turn="player2"
-  }
-}else{
-  //Create rect in top left
-  var rect = document.createElementNS(namespace, "rect")
-  rect.setAttribute("x",15)
-  rect.setAttribute("y" 0)
-  rect.setAttribute("width" 88)
-  rect.setAttribute("height" 90)
-  rect.setAttribute("fill","aqua" )
-  var canvas =document.getElementById("game-board")
-  canvas.appendChild(rect)
-  turn="player1"
-  }
 }
-
 
 
 function changeShape() {
@@ -48,7 +32,7 @@ function anotherShape() {
   circle.setAttribute("cx",255)
   circle.setAttribute("cy", 43)
   circle.setAttribute("r",25)
-  circle.setAttribute("fill","blue")
+  circle.setAttribute("fill","red")
   var canvas = document.getElementById("game-board")
   canvas.appendChild(circle)
 }
@@ -81,7 +65,7 @@ function MakeAnotherShape() {
   circle.setAttribute("cx",160)
   circle.setAttribute("cy", 148)
   circle.setAttribute("r",25)
-  circle.setAttribute("fill","blue")
+  circle.setAttribute("fill","red")
   var canvas = document.getElementById("game-board")
   canvas.appendChild(circle)
 }
@@ -92,7 +76,7 @@ function AnotherOne() {
   circle.setAttribute("cx",60)
   circle.setAttribute("cy", 245)
   circle.setAttribute("r",25)
-  circle.setAttribute("fill","blue")
+  circle.setAttribute("fill","red")
   var canvas = document.getElementById("game-board")
   canvas.appendChild(circle)
 }
@@ -114,16 +98,7 @@ function LastShape() {
   circle.setAttribute("cx",255)
   circle.setAttribute("cy", 245)
   circle.setAttribute("r",25)
-  circle.setAttribute("fill","blue")
+  circle.setAttribute("fill","red")
   var canvas = document.getElementById("game-board")
   canvas.appendChild(circle)
-}
-
-
-var turn= "player1"
-
-function  makeShape(){
-  if(turn=="player1"){
-
-  }
 }
